8715677eaf4d1f8dea59d9535e3f2c6fbe6be99c
Heiko Finzel <hfi@boreus.de>
2018-02-16 14:58:06 +0000

    root cmds to root.sh

--- users/root.sh
+++ users/root.sh
@@ -2,3 +2,13 @@
 function become {
     sudo -u $1 -i bash --rcfile $BOREUS_ENV/.bashrc
 }
+
+# stasi
+alias last='last -win 40'
+alias last2="grep '\-login:' /var/log/messages | grep -v 'ansible' | tail -n 40"
+
+# force refresh of cached stuff
+alias clearmycache="rm -f $BOREUS_ENV/.cache*"
+
+# Boreus stuff
+alias crc='/root/bin/mkMd5Sums && service netmodo restart'
